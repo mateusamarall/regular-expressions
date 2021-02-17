8d81393abbf722e2dfeb4a23d28c9515c2385cab
mateusamarall <mateus.amaral018@gmail.com>
2021-02-17 18:11:34 -0300

    match and replace

--- class2.js
+++ class2.js
@@ -12,9 +12,3 @@ console.log(texto.replace(/(João|Maria)/gi,function(input){
 
 // (...( () ) )(...)(...)  $1 $2 $3 $4 $5
   
-// console.log(texto);
-// console.log(texto.match(regExp1));
-// console.log(texto.replace(/(João|Maria)/gi, '<b>$1</b>'));
-// console.log(texto.replace(/(João|Maria)/gi, function(input) {
-//   return ' ############## ' + input.toUpperCase() + ' ############## ';
-// }));
