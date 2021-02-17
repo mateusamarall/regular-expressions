6689d548faf3dd9a34bb13f01bf2068c3f82d20e
mateusamarall <mateus.amaral018@gmail.com>
2021-02-17 16:12:01 -0300

    match and replace

---
+++ class2.js
@@ -0,0 +1,20 @@
+
+const { texto } = require('./base');
+const regExp1 = /João|Maria/gi;
+
+console.log(texto.match(regExp1));
+console.log(texto.replace(/(João|Maria)/gi,'<b>$1</b>'));
+console.log(texto.replace(/(João|Maria)/gi,function(input){
+  return ' ############## ' + input.toUpperCase() + ' ############## ';
+}));
+
+
+
+// (...( () ) )(...)(...)  $1 $2 $3 $4 $5
+  
+// console.log(texto);
+// console.log(texto.match(regExp1));
+// console.log(texto.replace(/(João|Maria)/gi, '<b>$1</b>'));
+// console.log(texto.replace(/(João|Maria)/gi, function(input) {
+//   return ' ############## ' + input.toUpperCase() + ' ############## ';
+// }));
