ef1c1d9cf792a1473b0899f594bb9df75f2b4b6b
mateusamarall <mateus.amaral018@gmail.com>
2021-03-29 20:45:47 -0300

    validação de cpf e ips

---
+++ class6.js
@@ -0,0 +1,22 @@
+const {cpfs, ips} = require('./base');
+
+// ^ -> começa com
+// $ -> termina com
+// console.log(cpfs.match(/[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/g));
+// console.log(cpfs.match(/\d{3}\.\d{3}\.\d{3}\-\d{2}/g));
+// console.log(cpfs.match(/(\d{3}\.){2}\d{3}\-\d{2}/g));
+
+//250 - 255 25[0-5]
+//200 - 249 2[0-4][0-9]
+//100 - 199 1\d{2}
+//10  - 99 [1-9]\d
+//0   - 9  \d
+const ipRegExp = /((25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]\d|\d)(\.)){3}(25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]\d|\d)/g;
+
+// for (let i = 0; i <=   255; i++) {                            
+//   const ip = `${i}.${i}.${i}.${i}`;
+//   console.log(ip, ip.match(ipRegExp));
+// }
+
+console.log(ips);
+console.log(ips.match(ipRegExp));
